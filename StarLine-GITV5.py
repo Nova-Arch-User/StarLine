f4be800ee251cd505f841b8478dad07fb80d9d85
NovaArch <oscargoodchild627@gmail.com>
2026-09-14 17:04:25 +0100

    Add files via upload

---
+++ StarLine-GITV5.py
@@ -0,0 +1,152 @@
+import subprocess
+import urllib.request
+import urllib.error
+import tarfile
+import os
+import sys
+import json
+import re  
+import shutil
+
+def BuildPackage(DirPath, IsMainPkg=True):
+  if IsMainPkg:
+     subprocess.run(["makepkg", "-si"], cwd=DirPath, check=True)
+  else:
+    
+     command = ["makepkg", "-si", "--noconfirm"]
+     subprocess.run(command, cwd=DirPath, check=True)
+
+USER_CACHE_BASE = os.path.join(os.path.expanduser("~"), ".cache", "StarLine")
+
+def download_AUR_Pkg(PkgName, IsMainPackage=True):
+ depenChoice = "Y" 
+ try:
+  pkg_cache_dir = os.path.join(USER_CACHE_BASE, PkgName)
+  if os.path.exists(pkg_cache_dir):
+     shutil.rmtree(pkg_cache_dir)
+  os.makedirs(pkg_cache_dir, exist_ok=True)
+  
+
+  
+  
+  Url = f"https://aur.archlinux.org/cgit/aur.git/snapshot/{PkgName}.tar.gz"
+  FileName = os.path.join(pkg_cache_dir, f"{PkgName}.tar.gz")
+  
+  print(f"Downloading {PkgName}...")
+  urllib.request.urlretrieve(Url, FileName)
+  OpenFile = tarfile.open(FileName)
+  if IsMainPackage:
+       depenChoice = input("Would you like to download AUR Dependencies? Y/n ").strip()
+       Ans = input("Would you like to read package build? y/N ")
+       if Ans == "y":
+      
+        with tarfile.open(FileName, "r:gz") as OpenFile:
+         AllFiles = OpenFile.getnames()
+         for File in AllFiles:
+          if File == f"{PkgName}/PKGBUILD":
+             OpenFile.extract(File, USER_CACHE_BASE)
+             PkgBuild_Path = os.path.join(USER_CACHE_BASE, File)
+             break   
+      
+        with open(PkgBuild_Path, "r") as file_stream:
+         print(file_stream.read())
+         AnsB = input("Would you like to procede? y/N ")
+        if AnsB != "y":
+         sys.exit()
+  OpenFile = tarfile.open(FileName)
+  OpenFile.extractall(path=USER_CACHE_BASE)
+  OpenFile.close()
+  os.remove(FileName)
+  
+  
+  if depenChoice == "Y" and IsMainPackage:
+    FoundDepends = DownloadDependencies(PkgName)
+    FoundDepends.reverse() 
+    for Depend in FoundDepends:
+      download_AUR_Pkg(Depend, IsMainPackage=False)
+
+
+  
+  
+  if not IsMainPackage:
+     ExtractedFolderPath = os.path.join(USER_CACHE_BASE, PkgName)
+     BuildPackage(ExtractedFolderPath, IsMainPkg=False)
+
+ except urllib.error.HTTPError as e:
+    if e.code == 404:
+      if IsMainPackage:
+            print("This package could not be found")
+            sys.exit()
+      else:
+            print(f"Skipping '{PkgName}': not on AUR, probably an official repo package")
+            return
+    else:
+      print(f"Server error, could not connect: {e.code} {e.reason}") 
+      sys.exit()
+
+def DownloadDependencies(PkgName):
+   PackageToCheck = [PkgName]
+   FoundDependencies = []
+   Seen = {PkgName}
+   
+   while len(PackageToCheck) > 0:
+       CurrentPkg = PackageToCheck.pop()
+       FoundDependencies.append(CurrentPkg)
+       UrlD = f"https://aur.archlinux.org/rpc/v5/info/{CurrentPkg}"
+       
+       try:
+           with urllib.request.urlopen(UrlD) as response:
+            JsonString = response.read().decode('utf-8')
+            PkgData = json.loads(JsonString)
+       except Exception:
+           continue
+           
+       if PkgData.get("resultcount", 0) > 0:
+           package_list = PkgData["results"]
+           if package_list:
+               Package_info = package_list[0]
+               
+               Depends = Package_info.get("Depends", [])
+               MakeDepends = Package_info.get("MakeDepends", [])
+               
+               Depends = Cleaner(Depends)
+               MakeDepends = Cleaner(MakeDepends)
+               Dependencies = Depends + MakeDepends
+               
+               NewDeps = [d for d in Dependencies if d not in Seen and d] 
+               Seen.update(NewDeps)
+               PackageToCheck.extend(NewDeps)
+               print(f"Package '{CurrentPkg}' needs: {Dependencies}")
+       else:
+           print(f"No depen found for {CurrentPkg}")
+           
+
+   if PkgName in FoundDependencies:
+      FoundDependencies.remove(PkgName)      
+   return FoundDependencies
+
+def Cleaner(RawData):
+   CleanList = []
+   for Data in RawData:
+   
+      CleanData = re.split(r'[=><:]', Data)[0].strip()
+      if CleanData:
+         CleanList.append(CleanData)
+   return CleanList
+
+if __name__ =="__main__":
+ TargetPackage = input("Please enter PkgName: ").strip()
+ while True:
+  try:
+   if TargetPackage:
+     download_AUR_Pkg(TargetPackage, IsMainPackage=True)
+     
+     
+     print("Download and installation complete!")
+     break
+   else:
+     print("Please enter a valid package name")
+  except KeyboardInterrupt:
+   print("Terminated")
+   break
+  
